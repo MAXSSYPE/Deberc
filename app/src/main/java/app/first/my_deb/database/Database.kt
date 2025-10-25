7de1e1e86ba649f213f88c2e5cf3630fbc73872a
Maksym Dudka <dudkamv@gmail.com>
2025-10-25 21:13:41 +0300

    Update 6.5.2

--- app/src/main/java/app/first/my_deb/database/Database.kt
+++ app/src/main/java/app/first/my_deb/database/Database.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [GameEntity::class, GamerEntity::class], version = 7)
+@Database(entities = [GameEntity::class, GamerEntity::class], version = 7, exportSchema = false)
 @TypeConverters(Convertor::class)
 abstract class Database : RoomDatabase() {
     abstract fun getDao(): Dao
